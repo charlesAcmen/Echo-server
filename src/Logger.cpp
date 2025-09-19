83f5247edeef17cb131274c3564b18022d02b7b0
charlesAcmen <15591542871@163.com>
2025-09-19 14:09:36 +0800

    Logger

--- src/Logger.cpp
+++ src/Logger.cpp
@@ -4,8 +4,8 @@ void Logger::INFO(const std::string& msg) {
     std::cout << "[INFO] " << msg << std::endl;
 }
 void Logger::WARN(const std::string& msg) {
-    std::cout << "[INFO] " << msg << std::endl;
+    std::cout << "[WARN] " << msg << std::endl;
 }
 void Logger::ERROR(const std::string& msg) {
-    std::cout << "[INFO] " << msg << std::endl;
+    std::cout << "[ERROR] " << msg << std::endl;
 }
